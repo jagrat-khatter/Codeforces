a5736cc8558c7501fa436e8f36d7307c66e7d065
jagrat-khatter <jagratkhatter@gmail.com>
2025-07-01 11:39:14 +0000

    Min Max MEX

--- jspwTest.cpp
+++ jspwTest.cpp
@@ -1,61 +1,60 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+ 
 using namespace std;
-using ll = long long;
-const ll lmin = LLONG_MIN;
-const ll lmax = LLONG_MAX;
-ll power(ll b , ll e){
-    if(e==0) return 1;
-
-    ll value = power(b , e/2);
-    if(e%2==0) return value * value;
-    else return value * value *  b;
+ 
+bool canSplit(const vector<int> &a, int k, int target) {
+	int n = a.size();
+	int segments = 0;
+	vector<int> count(target, 0);
+	int mexValue = 0;
+	for (int i = 0; i < n; ++i) {
+		if (a[i] < target) {
+			count[a[i]]++;
+			while (mexValue < target && count[mexValue] > 0) {
+				mexValue++;
+			}
+		}
+		if (mexValue == target) {
+			segments++;
+			fill(count.begin(), count.end(), 0);
+			mexValue = 0;
+		}
+	}
+	return segments >= k;
 }
-void func(vector<ll> v,set<ll> &s){
-    // if all the numbers in the vector are non negative means recusrsion has to end 
-    // cout << "control"<< '\n';
-    ll l =0;
-    while(v[l]!=-1){
-        l++;
-        if(l==v.size()) break;
-    }
-    // for(auto i : v) cout << i << ' ';
-    // cout << '\n';
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    if(l == v.size()){ // vector is complete
-       // cout << "complete" << '\n';
-        ll ct7 =0; ll ct4 =0;
-        ll mul = power(10 , v.size()-1);
-        ll num =0;
-        for(ll i=0;i<v.size();i++){
-            num += v[i] * mul;
-            if(v[i] == 4) ct4++;
-            else ct7++;
-
-            mul = mul/10;
-        }
-        // cout << num << '\n';
-        if(ct7 == ct4)s.insert(num);
-        return ;
-    }
-    else{ // vector is incomplete
-        v[l] = 4;
-        func(v , s);
-        v[l] = 7;
-        func(v , s);
-    }
-}
-signed main()
-{
-    // ios::sync_with_stdio(false);
-    // cin.tie(NULL);
-
-    vector<ll> v(6);
-    for(auto & i : v) i = -1;
-    // for(auto i : v) cout << i << ' ';
-    set<ll> s;
-    func(v , s);
-    
-
-
-    return 0;
+	int t;
+	cin >> t;
+ 
+	while (t--) {
+		int n, k;
+		cin >> n >> k;
+ 
+		vector<int> a(n);
+		for (int i = 0; i < n; ++i) {
+			cin >> a[i];
+		}
+ 
+		int left = 0, right = n + 1;
+		int result = 0;
+		while (left < right) {
+			int mid = left + (right - left + 1) / 2;
+			if (canSplit(a, k, mid)) {
+				result = mid;
+				left = mid;
+			} else {
+				right = mid - 1;
+			}
+		}
+ 
+		cout << result << '\n';
+	}
+ 
+	return 0;
 }
